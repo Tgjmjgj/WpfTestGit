619b2c03e25b14d885dccd573be01a14cf061672
Pavel Karpovich <Chesh397@mail.ru>
2019-02-08 11:48:18 +0300

    Добавлен клик по кнопке

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfTestGit
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("dfesfesf");
+        }
     }
 }
